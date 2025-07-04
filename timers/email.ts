f304ab16ebbc12426111840f8d3488c4ded94515
Teemu L. <teemulaaks@gmail.com>
2025-07-04 12:14:26 +0300

    change sendgrid to nodemailer

--- timers/email.ts
+++ timers/email.ts
@@ -1,15 +1,28 @@
-import sgMail from "@sendgrid/mail";
 import { ITimedMessage } from "./timers";
 import "dotenv/config";
 import dayjs from "dayjs";
-sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+import nodemailer from "nodemailer";
 
-export const sendMail = (timedMessage: ITimedMessage) => {
-  console.log(timedMessage)
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+export const sendMail = async (timedMessage: ITimedMessage) => {
+  console.log(timedMessage);
   let emailText = timedMessage.content;
 
   if (timedMessage.sendLocation) {
-    emailText += ` ${dayjs(timedMessage.location.timestamp).format('DD-MM-YYYY HH:mm:ss')} Latitude: ${timedMessage.location.coords.latitude} Longitude: ${timedMessage.location.coords.longitude}`;
+    emailText += ` ${dayjs(timedMessage.location.timestamp).format(
+      "DD-MM-YYYY HH:mm:ss"
+    )} Latitude: ${timedMessage.location.coords.latitude} Longitude: ${
+      timedMessage.location.coords.longitude
+    }`;
   }
 
   const emailMessage = {
@@ -19,9 +32,9 @@ export const sendMail = (timedMessage: ITimedMessage) => {
     text: emailText,
   };
   try {
-    sgMail.send(emailMessage);
-    console.log(`sent ${emailMessage}`)
+    await transporter.sendMail(emailMessage);
+    console.log(`sent ${emailMessage}`);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 };
